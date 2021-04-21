b6dbf75271b13d8ec8a85827057155af939fbee2
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2021-04-21 14:22:18 +0700

    using variable

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Planet from "./iterables/Planet";
 
-const fetchPlanets = async () => {
-  const res = await fetch("http://swapi.dev/api/planets/");
+const fetchPlanets = async (data) => {
+  console.log(data);
+  const res = await fetch(
+    `http://swapi.dev/api/planets/?page=${data.queryKey[1]}`
+  );
   return res.json();
 };
 
 export const Planets = () => {
-  const { data, status } = useQuery("planets", fetchPlanets, {
-    staleTime: 2000,
-  });
-  console.log(data);
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(["planets", page], fetchPlanets);
   return (
     <>
       <div className="font-bold uppercase mt-5">Planets</div>
       <p className="mb-5">List of Planet</p>
+      <div className="flex items-center mb-5 mr-4 space-x-2">
+        <p>Page:</p>
+        <button
+          onClick={() => setPage(1)}
+          className="px-3 py-1 bg-gray-700 border-gray-800"
+        >
+          1
+        </button>
+        <button
+          onClick={() => setPage(2)}
+          className="px-3 py-1 bg-gray-700 border-gray-800"
+        >
+          2
+        </button>
+        <button
+          onClick={() => setPage(3)}
+          className="px-3 py-1 bg-gray-700 border-gray-800"
+        >
+          3
+        </button>
+      </div>
       {status === "error" && <div>Error fetching data</div>}
       {status === "loading" && <div>Loading...</div>}
       {status === "success" && (
